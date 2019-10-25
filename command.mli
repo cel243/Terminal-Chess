b7aafb7e4d0c4f14bbf933594679526f3362328f
cel243 <cel243@cornell.edu>
2019-10-25 15:04:26 -0400

    command interface

---
+++ command.mli
@@ -0,0 +1,23 @@
+(** This module parses player commands in the chess game *)
+
+(** the type of a location on the chess board, where the columns
+    are labeled A..H from left to right, and the rows are labelled 
+    1..8 from bottom to top *)
+type location = char * int 
+
+(** the type of a player command *)
+type command = 
+  | Quit 
+  | Draw 
+  | Restart 
+  | Move of location*location
+  | Take of location*location 
+
+(** raised when the command isn't one of the expected forms  *)
+exception Invalid 
+
+(** TODO *)
+val parse : string -> command 
+
+
+
