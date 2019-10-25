14724edd4f5e5db3aaeb824eaea0900defc47433
cel243 <cel243@cornell.edu>
2019-10-25 14:49:49 -0400

    hopefully finished board interface

--- Board.mli
+++ Board.mli
@@ -6,37 +6,53 @@
 
     It contains functions to mutate the state of the board.  *)
 
-(** The abstract type of the value representing the board itself,
+(** The abstract type of the value representing the chess game,
     which contains information about where each piece is located. *)
 type t 
 
-type piece = Pawn | Rook (*  | .... *)
+(** the type of a chess piece, where the piece is one of 
+    the 6 standard chess peices *)
+type piece = Pawn | Rook | Bishop | Knight | Queen | King 
+
+(** the color black or white *)
 type color = Black | White 
+
+(** the type of a piece in the chess game, containing a type, 
+    a color denoting which player the piece belongs to, 
+    and whether the piece has been moved previously.  *)
 type game_piece = {p_type : piece; col : color; has_moved : bool }
 
-(** [init_state] is the initial state of the board when a chess
+(** [init_state] is the initial state of the game when a chess
     game is started. *)
 val init_state : t 
 
-(** [get_piece_at b c i] is [Some p], where [p] is the chess piece at c,i on 
-    board [b], if such a piece exists, where the board is labelled A..H from 
-    left to right, and 1..8 from bottom to top. If there is no piece at that 
-    location, [get_piece_at c i] is [None]. *)
+(** [get_current_player g] is [White] if it is the white player's
+    turn to move in game [g] and [Black] otherwise *)
+val get_current_player : t -> color 
+
+(** [update_current_player g] updates the color of the current player 
+    so that it becomes the opposite color. *)
+val update_current_player : t -> unit 
+
+(** [get_piece_at g c i] is [Some p], where [p] is the chess piece at c,i on
+    the board of game [g], if such a piece exists, where the board is 
+    labelled A..H from left to right, and 1..8 from bottom to top. 
+    If there is no piece at that location, [get_piece_at c i] is [None]. *)
 val get_piece_at : t -> char -> int -> game_piece option 
 
-(** [get_white_pieces b] is the list of all game pieces labelled as
-    [White] on board [b], where each item of the list is of the form 
+(** [get_white_pieces g] is the list of all game pieces labelled as
+    [White] in game [g], where each item of the list is of the form 
     [(p, c, i)], where [p] is the game piece, [c] is the char location
     of this piece, and [i] is the int location.   *)
 val get_white_pieces : t -> (game_piece * char * int) list 
 
-(** [get_black_pieces b] is the list of all game pieces labelled as
-    [Black] on board [b], where each item of the list is of the form 
+(** [get_black_pieces g] is the list of all game pieces labelled as
+    [Black] in game [g], where each item of the list is of the form 
     [(p, c, i)], where [p] is the game piece, [c] is the char location
     of this piece, and [i] is the int location.   *)
 val get_black_pieces : t -> (game_piece * char * int) list 
 
-(** [move_piece b c1 i1 c2 i2] is board [b] after the piece at
+(** [move_piece g c1 i1 c2 i2] updates the board of chess game [g] so that 
     [c1, i1] on the board has replaced whatever was previously at
     [c2, i2] on the board , leaving an empty square at [c1, i1]. *)
-val move_piece : t -> char -> int -> char -> int -> t 
+val move_piece : t -> char -> int -> char -> int -> unit 
