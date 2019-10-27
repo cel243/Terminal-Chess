97ba900e3912fa676ff83e853a929891bc2abe8c
cel243 <cel243@cornell.edu>
2019-10-27 17:14:42 -0400

    test suite

--- test.ml
+++ test.ml
@@ -42,9 +42,9 @@ let command_tests = [
   "'   DrAw  ' is parsed as Draw" >:: 
   (fun _ -> assert_equal Draw (parse "    DrAw  ")); 
   "'A6 to B4' is parsed as Move ('A',6,'B',4)" >:: 
-  (fun _ -> assert_equal Move ('A',6,'B',4) (parse "A6 to B4")); 
+  (fun _ -> assert_equal (Move ('A',6,'B',4)) (parse "A6 to B4")); 
   "'  b3  TO  c8' is parsed as Move ('B',3,'C',8)" >:: 
-  (fun _ -> assert_equal Move ('B',3,'C',8) (parse "  b3  TO  c8")); 
+  (fun _ -> assert_equal (Move ('B',3,'C',8)) (parse "  b3  TO  c8")); 
 ] 
 
 let suite =
