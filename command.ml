8855c2896d59661c941bb388994aa5b59b2b8f00
cel243 <cel243@cornell.edu>
2019-10-27 17:21:20 -0400

    tests pass

--- command.ml
+++ command.ml
@@ -11,13 +11,14 @@ let get_command wrd_ls =
   | ["QUIT"] -> Quit 
   | ["DRAW"] -> Draw 
   | [l1; "TO"; l2] when String.length l1 = 2 && String.length l2 = 2 -> 
-    Move (String.get l1 0, int_of_char (String.get l1 1),
-          String.get l2 0, int_of_char (String.get l2 1) )
+    Move (String.get l1 0, int_of_char (String.get l1 1) - 48,
+          String.get l2 0, int_of_char (String.get l2 1) - 48 )
   | _ -> raise Invalid  
 
 (* val parse : string -> t  *)
 let parse p_in = 
   String.split_on_char ' ' p_in 
+  |> List.filter (fun x -> x <> "") 
   |> List.map (fun s -> String.uppercase_ascii s)
   |> get_command  
 
