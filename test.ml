0d5bd5708337b5cd09c5241a9221f0ead63e9015
fcg26 <fcg26@cornell.edu>
2019-10-27 17:08:08 -0400

    more board test cases

--- test.ml
+++ test.ml
@@ -2,6 +2,7 @@ open OUnit2
 open Board
 open Command 
 
+let game_state = init_state
 
 let board_tests = [
   "The starting player is White" >:: 
@@ -16,12 +17,15 @@ let board_tests = [
   "The piece at A1 is a White Rook that hasn't moved" >:: 
   (fun _ -> assert_equal {p_type = Rook; col = White; has_moved = false} 
       (get_piece_at init_state 'A' 1)); 
-  "'   DrAw  ' is parsed as Draw" >:: 
+  "White has 16 pieces" >:: 
   (fun _ -> assert_equal 16 ((get_white_pieces init_state).length)); 
-  "'A6 to B4' is parsed as Move ('A',6,'B',4)" >:: 
-  (fun _ -> assert_equal Move ('A',6,'B',4) (parse "A6 to B4")); 
-  "'  b3  TO  c8' is parsed as Move ('B',3,'C',8)" >:: 
-  (fun _ -> assert_equal Move ('B',3,'C',8) (parse "  b3  TO  c8")); 
+  "Black has 16 pieces" >:: 
+  (fun _ -> assert_equal 16 ((get_black_pieces init_state).length)); 
+  "The White rook has taken the Black Rook" >:: 
+  (fun _ -> assert_equal {p_type = Rook; col = White; has_moved = true} 
+      (get_piece_at game_state H 8 (move_piece game_state A 1 H 8))); 
+  "Black has 15 pieces" >:: 
+  (fun _ -> assert_equal 15 ((get_black_pieces game_state).length));  
 ] 
 
 
