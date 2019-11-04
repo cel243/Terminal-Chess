dcdf35daa0c7bcc25829bf093004c66688ae4db0
cel243 <cel243@cornell.edu>
2019-11-04 17:37:31 -0500

    tests fixed

--- test.ml
+++ test.ml
@@ -57,33 +57,52 @@ let () = Board.next_player logic_GS_Black
 let logic_king = Board.init_state ()   
 let () = Board.move_piece logic_king 'D' 8 'A' 5
 
+let logic_king_pawn = Board.init_state ()   
+let () = Board.move_piece logic_king_pawn 'D' 8 'A' 5
+let () = Board.move_piece logic_king_pawn 'D' 2 'D' 3
+
+let piece_move_pawn = Board.init_state ()   
+let () = Board.move_piece piece_move_pawn 'D' 2 'D' 3
+let () = Board.move_piece piece_move_pawn 'C' 7 'C' 5
+
+let piece_move_rook = Board.init_state ()   
+let () = Board.move_piece piece_move_rook 'A' 1 'D' 4
+
 let piece_move = Board.init_state () 
+
+let print_logic_res = function 
+  | Legal -> "legal"
+  | Illegal str -> "illegal "^str 
+  | Terminate -> "terminate" 
+
+let print_col = function 
+  | White -> "white"
+  | Black -> "black"
+
 let logic_tests = [
 
   (* Tests of is_blocked *)
-  (* "logic says move C2 to C3 is legal" >:: (fun _ -> 
+  "logic says move C2 to C3 is legal" >:: (fun _ -> 
       assert_equal Legal (process logic_GS (Move ('C',2,'C',3))));
-     "logic moved c2 to c3 in the previous test" >:: (fun _ -> 
+  "logic moved c2 to c3 in the previous test" >:: (fun _ -> 
       assert_equal (Some {p_type=Pawn;col=White;has_moved=true})
         (get_piece_at logic_GS 'C' 3));
-     "logic refuses to move A1 to A3 because the rook is blocked" >:: 
-     (fun _ -> assert_equal (Illegal "") (process logic_GS (Move ('A',1,'A',3))));
-     "logic did not move the rook" >:: 
-     (fun _ -> assert_equal None (get_piece_at logic_GS 'A' 4)); 
-     "logic refuses to move C8 to F5 because the bishop is blocked" >:: 
-     (fun _ -> assert_equal (Illegal "") (process logic_GS (Move ('C',8,'F',5))));
-     "logic did not move the rook" >:: 
-     (fun _ -> assert_equal None (get_piece_at logic_GS 'F' 5)); 
-     "logic says move D7 to D6 is legal" >:: (fun _ -> 
-      assert_equal Legal (process logic_GS (Move ('D',7,'D',6))));
-     "logic moved d7 to d6 in the previous test" >:: (fun _ -> 
-      assert_equal (Some {p_type=Pawn;col=Black;has_moved=true})
-        (get_piece_at logic_GS 'D' 6));
-     "logic now allows bishop to move" >:: (fun _ -> 
-      assert_equal Legal (process logic_GS (Move ('C',8,'F',5))));
-     "logic moved bishop" >:: (fun _ -> 
-      assert_equal (Some {p_type=Bishop;col=Black;has_moved=true})
-        (get_piece_at logic_GS 'F' 5)); *)
+  "logic refuses to move A1 to A3 because the rook is blocked" >:: 
+  (fun _ -> assert_equal (Illegal "This piece is blocked!") 
+      (process logic_GS (Move ('A',1,'A',3))));
+  "logic did not move the rook" >:: 
+  (fun _ -> assert_equal None (get_piece_at logic_GS 'A' 4)); 
+  "logic did not move the rook" >:: 
+  (fun _ -> assert_equal None (get_piece_at logic_GS 'F' 5)); 
+  "logic refuses to move C8 to F5 because the bishop is blocked" >:: 
+  (fun _ -> assert_equal (Illegal "This piece is blocked!") 
+      (process logic_GS_Black (Move ('C',8,'F',5))) 
+      ~printer: print_logic_res);
+  "logic says move D7 to D6 is legal" >:: (fun _ -> 
+      assert_equal Legal (process logic_GS_Black (Move ('D',7,'D',6)))
+        ~printer: print_logic_res);
+  "logic now allows bishop to move" >:: (fun _ -> 
+      assert_equal Legal (process logic_GS_Black (Move ('C',8,'F',5))));
 
   (** out of bounds tests *)
   "logic says move to out-of-bounds (file) is illegal" >:: (fun _ -> 
@@ -108,71 +127,56 @@ let logic_tests = [
         (process logic_GS (Move ('A',4,'A',5))));
 
 
-  "logic refuses to move A1 to A3 because the rook is blocked" >:: 
-  (fun _ -> assert_equal (Illegal "This piece is blocked!") 
-      (process logic_GS (Move ('A',1,'A',3))));
-  "logic did not move the rook" >:: 
-  (fun _ -> assert_equal None (get_piece_at logic_GS 'A' 4)); 
-  "logic refuses to move C8 to F5 because the bishop is blocked" >:: 
-  (fun _ -> assert_equal (Illegal "This piece is blocked!") 
-      (process logic_GS_Black (Move ('C',8,'F',5))));
-  "logic did not move the rook" >:: 
-  (fun _ -> assert_equal None (get_piece_at logic_GS_Black 'F' 5)); 
-  "logic says move D7 to D6 is legal" >:: (fun _ -> 
-      assert_equal Legal (process logic_GS_Black (Move ('D',7,'D',6))));
-  "logic moved d7 to d6 in the previous test" >:: (fun _ -> 
-      assert_equal (Some {p_type=Pawn;col=Black;has_moved=true})
-        (get_piece_at logic_GS_Black 'D' 6));
-  "logic now allows bishop to move" >:: (fun _ -> 
-      assert_equal Legal (process logic_GS_Black (Move ('C',8,'F',5))));
-  "logic moved bishop" >:: (fun _ -> 
-      assert_equal (Some {p_type=Bishop;col=Black;has_moved=true})
-        (get_piece_at logic_GS_Black 'F' 5));
-
   (* Tests of detecting check *)
-  (* "logic will not let pawn move because leaves king in check" >:: 
-     (fun _ -> assert_equal (Illegal "You can't leave your king in check!") 
-      (process logic_king (Move ('D',2,'D',3)))); *)
+  "logic will not let pawn move because leaves king in check" >:: 
+  (fun _ -> assert_equal (Illegal "You can't leave your king in check!") 
+      (process logic_king (Move ('D',2,'D',3)))
+      ~printer: print_logic_res) ;
 
-  (let () = Board.move_piece logic_king 'D' 2 'D' 3 in 
-   "if pawn moves, no white piece can move that doesn't stop check" >:: 
-   (fun _ -> assert_equal (Illegal "You can't leave your king in check!") 
-       (process logic_king (Move ('A',2,'A',3)))));
+  "if pawn moves, no white piece can move that doesn't stop check" >:: 
+  (fun _ -> assert_equal (Illegal "You can't leave your king in check!") 
+      (process logic_king_pawn (Move ('A',2,'A',3))));
 
-  (* "can move piece that prevents check" >:: 
-     (fun _ -> assert_equal Legal (process logic_king (Move ('A',2,'A',3)))); *)
+  "can move piece that prevents check" >:: 
+  (fun _ -> assert_equal Legal (process logic_king_pawn (Move ('C',2,'C',3)))
+      ~printer: print_logic_res);
 
 
   (* tests of piece movement *)
   (* PAWN *)
-  (* "can move pawn forward 1" >:: 
-     (fun _ -> assert_equal Legal (process piece_move (Move ('A',2,'A',3))));
-     (let () = Board.move_piece piece_move 'A' 3 'D' 4 in 
-     "cannot move pawn backwards" >:: 
-     (fun _ -> assert_equal (Illegal  "This piece can't move like that!")
-       (process piece_move (Move ('D',4,'D',3)))));
-     "cannot move pawn sideways" >:: 
-     (fun _ -> assert_equal (Illegal  "This piece can't move like that!")
-      (process piece_move (Move ('D',4,'E',3))));
-     "cannot move pawn diagonally" >:: 
-     (fun _ -> assert_equal (Illegal  "This piece can't move like that!")
-      (process piece_move (Move ('D',4,'E',5)))); 
+  "can move pawn forward 1" >:: 
+  (fun _ -> assert_equal Legal (process piece_move_pawn (Move ('D',3,'D',4)))
+      ~printer: print_logic_res);
+  "cannot move pawn backwards" >:: 
+  (fun _ -> assert_equal (Illegal  "This piece can't move like that!")
+      (process piece_move_pawn (Move ('D',4,'D',3)))
+      ~printer: print_logic_res);
+  "cannot move pawn sideways" >:: 
+  (fun _ -> assert_equal (Illegal  "This piece can't move like that!")
+      (process piece_move_pawn (Move ('D',4,'E',3)))
+      ~printer: print_logic_res);
+  "cannot move pawn diagonally" >:: 
+  (fun _ -> assert_equal (Illegal  "This piece can't move like that!")
+      (process piece_move_pawn (Move ('D',4,'E',5)))
+      ~printer: print_logic_res); 
+  "pawn can move diagonally when taking other piece" >:: 
+  (fun _ -> assert_equal Legal (process piece_move_pawn (Move ('D',4,'C',5)))
+      ~printer: print_logic_res) ;
 
-     (let () = Board.move_piece piece_move 'E' 7 'E' 5 in 
-     "pawn can move diagonally when taking other piece" >:: 
-     (fun _ -> assert_equal Legal (process piece_move (Move ('D',4,'E',5)))));
-     (* ROOK *)
-     (let piece_move = Board.init_state () in 
-     let () = Board.move_piece piece_move 'A' 1 'D' 4 in 
-     "can move rook forward 2" >:: 
-     (fun _ -> assert_equal Legal (process piece_move (Move ('D',4,'D',6)))));
-     "can move rook to the right" >:: 
-     (fun _ -> assert_equal Legal (process piece_move (Move ('D',4,'H',4))));
 
-     "can move rook to the left" >:: 
-     (fun _ -> assert_equal Legal (process piece_move (Move ('D',4,'A',4))));
-     "can move rook backwards" >:: 
-     (fun _ -> assert_equal Legal (process piece_move (Move ('D',4,'D',3)))); *)
+  (* ROOK *)
+  "can move rook forward 2" >:: 
+  (fun _ -> assert_equal Legal 
+      (process (copy_board piece_move_rook) (Move ('D',4,'D',6))));
+  "can move rook to the right" >:: 
+  (fun _ -> assert_equal Legal 
+      (process (copy_board piece_move_rook)  (Move ('D',4,'H',4))));
+  "can move rook to the left" >:: 
+  (fun _ -> assert_equal Legal 
+      (process (copy_board piece_move_rook) (Move ('D',4,'A',4))));
+  "can move rook backwards" >:: 
+  (fun _ -> assert_equal Legal 
+      (process (copy_board piece_move_rook) (Move ('D',4,'D',3))));
 ]
 
 let suite =
